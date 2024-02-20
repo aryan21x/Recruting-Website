5d9f14b1360c215fb1ebf62536b2d50d5428eab7
Preister, Kaleb <Kaleb.Preister@mines.sdsmt.edu>
2024-02-19 18:45:22 -0700

    Ref #4 fix for merge conflicts

--- Team18/Team18/Program.cs
+++ Team18/Team18/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Team18.Data;
+using Team18.Models;
+//using Team18.Data.SeedData;
+
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("Team18ContextConnection") ?? throw new InvalidOperationException("Connection string 'Team18ContextConnection' not found.");
 
@@ -10,6 +14,11 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Team18Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Team18Context") ?? throw new InvalidOperationException("Connection string 'Team18Context' not found.")));
+
 
 var app = builder.Build();
 
