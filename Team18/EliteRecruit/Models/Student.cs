70611e7537fcd00452a68274cfa0c596332f2f23
Preister, Kaleb <Kaleb.Preister@mines.sdsmt.edu>
2024-03-05 22:30:56 -0700

    Ref #33 removed some of the question marks in the models to remove warnings

--- Team18/EliteRecruit/Models/Student.cs
+++ Team18/EliteRecruit/Models/Student.cs
@@ -8,24 +8,24 @@ namespace EliteRecruit.Models
         public int Id { get; set; }
 
         [Required]
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
         [Required]
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
         [Required]
-        public string? School { get; set; }
+        public string School { get; set; }
 
         [Required]
         public decimal GPA { get; set; }
 
         [Required]
-        public string? Major { get; set; }
+        public string Major { get; set; }
 
         [Required]
-        public string? SchoolYear { get; set; }
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string SchoolYear { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
 
         public int GraduationYear
         {
