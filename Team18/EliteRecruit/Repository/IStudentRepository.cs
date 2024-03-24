e7e11a2e947669e0780fe4460c8523825f397c8b
Preister, Kaleb <Kaleb.Preister@mines.sdsmt.edu>
2024-03-24 10:59:36 -0600

    Ref #9 implemented repository pattern but not using it yet

---
+++ Team18/EliteRecruit/Repository/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using EliteRecruit.Models;
+using EliteRecruit.ViewModels;
+using static EliteRecruit.Helpers.Enums;
+
+namespace EliteRecruit.Interfaces
+{
+    public interface IStudentRepository : IDisposable
+    {
+        Task<IList<Student>> GetStudents(string filterBy, SortByParameter sortBy);
+        //Task<Student> GetStudentById(int studentId);
+        //Task<Student> GetStudentByName(string name);
+        //Task<Student> GetStudentByEmail(string email);
+        Task<Student> InsertStudent(StudentViewModel studentViewModel);
+        Task<Student> UpdateStudent(StudentViewModel studentViewModel);
+        Task DeleteStudent(int studentId);
+    }
+}
---
+++ Team18/EliteRecruit/Repository/StudentRepository.cs
@@ -0,0 +1,147 @@
+﻿using EliteRecruit.Data;
+using EliteRecruit.Interfaces;
+using EliteRecruit.Models;
+using EliteRecruit.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using static EliteRecruit.Helpers.Enums;
+
+namespace EliteRecruit.Repository
+{
+    public class StudentRepository(EliteRecruitContext context) : IStudentRepository
+    {
+        private EliteRecruitContext _context = context;
+
+        /*public StudentRepository(Student context)
+        {
+            this.context = context;
+        }*/
+
+        public async Task<IList<Student>> GetStudents(string filterBy, SortByParameter sortBy)
+        {
+            var students = from s in _context.Student
+                           select s;
+
+            if (string.IsNullOrEmpty(filterBy) == false) 
+            {
+                students = students.Where(s => s.FirstName.Contains(filterBy) || s.LastName.Contains(filterBy));
+            }
+
+            students = sortBy switch
+            {
+                SortByParameter.FirstNameDESC => students.OrderByDescending(o => o.FirstName).ThenByDescending(o => o.LastName),
+                SortByParameter.LastNameASC => students.OrderBy(o => o.LastName).ThenBy(o => o.FirstName),
+                SortByParameter.LastNameDESC => students.OrderByDescending(o => o.LastName).ThenByDescending(o => o.FirstName),
+                SortByParameter.GraduationDateASC => students.OrderBy(o => o.GraduationYear).ThenBy(o => o.FirstName).ThenBy(o => o.LastName),
+                SortByParameter.GraduationDateDESC => students.OrderByDescending(o => o.GraduationYear).ThenByDescending(o => o.FirstName).ThenByDescending(o => o.LastName),
+                _ => students.OrderBy(o => o.FirstName).ThenBy(o => o.LastName).ThenBy(o => o.GraduationYear),
+            };
+
+            return await students.ToListAsync();
+        }
+
+        public async Task<Student> GetStudentByID(int studentId)
+        {
+            return await _context.Student.FirstOrDefaultAsync(s => s.Id == studentId);
+        }
+
+        public async Task<Student> InsertStudent(StudentViewModel studentViewModel)
+        {
+            Student student = new()
+            {
+                //FirstName = studentViewModel.FirstName.Trim(),
+                //LastName = studentViewModel.LastName.Trim(),
+                //GraduationDate = studentViewModel.GraduationDate
+                FirstName = studentViewModel.FirstName.Trim(),
+                LastName = studentViewModel.LastName.Trim(),
+                School = studentViewModel.School.Trim(),
+                GPA = studentViewModel.GPA,
+                Major = studentViewModel.Major.Trim(),
+                SchoolYear = studentViewModel.SchoolYear,
+                Email = studentViewModel.Email,
+                PhoneNumber = studentViewModel.PhoneNumber
+            };
+
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+
+            return student;
+        }
+
+        public async Task DeleteStudent(int studentID)
+        {
+            var student = _context.Student.SingleOrDefault(s => s.Id == studentID);
+
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Student> UpdateStudent(StudentViewModel studentViewModel)
+        {
+            Student student;
+
+            try
+            {
+                student = await _context.Student.FindAsync(studentViewModel.Id);
+
+                //student.FirstName = studentViewModel.FirstName.Trim();
+                //student.LastName = studentViewModel.LastName.Trim();
+                //student.GraduationDate = studentViewModel.GraduationDate;
+                student.FirstName = studentViewModel.FirstName.Trim();
+                student.LastName = studentViewModel.LastName.Trim();
+                student.School = studentViewModel.School.Trim();
+                student.GPA = studentViewModel.GPA;
+                student.Major = studentViewModel.Major.Trim();
+                student.SchoolYear = studentViewModel.SchoolYear;
+                student.Email = studentViewModel.Email;
+                student.PhoneNumber = studentViewModel.PhoneNumber;
+
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(studentViewModel.Id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return student;
+        }
+
+        private bool StudentExists(int id)
+        {
+            return (_context.Student?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
